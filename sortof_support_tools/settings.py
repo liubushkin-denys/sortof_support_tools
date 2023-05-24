1ea491a9e28fe94f692560a7f6809515a6a4bfd0
ratpiss <disstray@protonmail.com>
2023-05-25 00:57:09 +0300

    fixed the issue with template not found

--- sortof_support_tools/settings.py
+++ sortof_support_tools/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = ["192.168.0.120","dlsupptools.fun", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    'tools.apps.ToolsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'sortof_support_tools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tools/views.py
+++ tools/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world. MX Converter here.")
+    template = loader.get_template("tools/index.html")
+    context = { 
+        
+    }
+    return HttpResponse(template.render(context, request))
