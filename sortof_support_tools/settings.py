e3638a94cbb3ad9ba2e85328bcd850db1a656416
ratpiss <disstray@protonmail.com>
2023-05-29 22:25:05 +0300

    static

--- sortof_support_tools/settings.py
+++ sortof_support_tools/settings.py
@@ -121,15 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATIC_URL = 'static/'
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'sortof_support_tools/static/')
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 
 #STATICFILES_DIRS = (
 #    os.path.join(SITE_ROOT, 'static/'),
